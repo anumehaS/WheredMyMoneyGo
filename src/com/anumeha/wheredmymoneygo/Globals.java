d203746b2c6a57f4d721ebbe98dd1ac3e0505706
anumehaS <anumeha.srivastava89@gmail.com>
2014-10-06 23:51:58 -0700

    Added globals file

---
+++ src/com/anumeha/wheredmymoneygo/Globals.java
@@ -0,0 +1,10 @@
+package com.anumeha.wheredmymoneygo;
+
+public class Globals {
+
+	public static final String INTERNAL_DATE_FORMAT = "yyyy-MM-dd";
+	public static final String USER_DATE_FORMAT = "MMMM dd, yyyy";
+	
+	//shared preferences variables
+	public static final String NUM_ALARMS = "num_alarms";
+}
